27a72151a8c41d8e7e211f7743b2b94ca7e34f1d
barren-way <xiny_liu@126.com>
2020-12-17 15:41:23 +0800

    完成商品模型的建立，并完成商品控制器及视图的构建，实现了新建一项新的商品，并可在后台查看现有商品

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,32 @@
 class Product < ApplicationRecord
-	
-	belongs_to :category
-	before_create :set_default_attrs
 
+  validates :category_id, presence: { message: "分类不能为空" }
+  validates :title, presence: { message: "名称不能为空" }
+  validates :status, inclusion: { in: %w[on off], 
+    message: "商品状态必须为on | off" }
+  validates :amount, numericality: { only_integer: true,
+    message: "库存必须为整数" },
+    if: proc { |product| !product.amount.blank? }
+  validates :amount, presence: { message: "库存不能为空" }
+  validates :msrp, presence: { message: "MSRP不能为空" }#原🏠,即市场0⃣️售价格
+  validates :msrp, numericality: { message: "MSRP必须为数字" },
+    if: proc { |product| !product.msrp.blank? }
+  validates :price, numericality: { message: "价格必须为数字" },
+    if: proc { |product| !product.price.blank? }
+  validates :price, presence: { message: "价格不能为空" }
+  validates :description, presence: { message: "描述不能为空" }
 
+  belongs_to :category
 
-	private
-	def set_default_attrs
-		self.uuid = RandomCode.generate_product_uuid
-	end
+  before_create :set_default_attrs
 
+  module Status
+    On = 'on'
+    Off = 'off'
+  end
+
+  private
+  def set_default_attrs
+    self.uuid = RandomCode.generate_product_uuid
+  end
 end
