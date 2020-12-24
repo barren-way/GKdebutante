6e10c3680ec57394a9d2f82950311b9d1ca98461
hetianyu <tianyu.he@aliyun.com>
2020-12-24 11:58:15 +0800

    商品详情页面

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   
   def index
-    @categories = Category.grouped_data
+    fetch_home_data
     
     @products = Product.onshelf.page(params[:page] || 1).per_page(params[:per_page] || 12)
       .order("id desc").includes(:main_product_image)
