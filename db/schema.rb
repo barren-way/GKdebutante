997c8bac418ec1aa81811d067302cbc77ff04bd9
hetianyu <tianyu.he@aliyun.com>
2020-12-25 21:47:46 +0800

    购物车模型及用户模型修改

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_025309) do
+ActiveRecord::Schema.define(version: 2020_12_25_134301) do
 
   create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,17 @@ ActiveRecord::Schema.define(version: 2020_12_24_025309) do
     t.index ["uuid"], name: "index_products_on_uuid", unique: true
   end
 
+  create_table "shopping_carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "user_uuid"
+    t.integer "product_id"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+    t.index ["user_uuid"], name: "index_shopping_carts_on_user_uuid"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -68,10 +79,12 @@ ActiveRecord::Schema.define(version: 2020_12_24_025309) do
     t.string "activation_state"
     t.string "activation_token"
     t.datetime "activation_token_expires_at"
+    t.string "uuid"
     t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
 end
