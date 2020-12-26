b9dd08cc7e84364b7d0c4527961055069cfa792f
hetianyu <tianyu.he@aliyun.com>
2020-12-26 23:21:05 +0800

    购物车功能

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'ancestry'
 gem 'will_paginate'
 gem 'paperclip', '~> 5.0.0'
 
+gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
@@ -21,6 +22,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
