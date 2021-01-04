bc347260a92d37a3b548eaa4d5520378ebc2eb4e
EddieAndHisCpp <72387839+EddieAndHisCpp@users.noreply.github.com>
2021-01-04 20:44:04 +0800

    手机验证码注册登录功能开发

---
+++ config/routes.rb
@@ -0,0 +1,48 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  resources :users
+  resources :sessions
+  delete '/logout' => 'sessions#destroy', as: :logout
+
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
+  resources :shopping_carts
+  resources :addresses do
+    member do
+      put :set_default_address
+    end
+  end
+  resources :orders
+  resources :payments, only: [:index] do
+    collection do
+      get :generate_pay
+      get :pay_return
+      get :pay_notify
+      get :success
+      get :failed
+    end
+  end
+  resources :cellphone_tokens, only: [:create]
+
+  namespace :dashboard do
+    scope 'profile' do
+      controller :profile do
+        get :password
+        put :update_password
+      end
+    end
+
+    resources :orders, only: [:index]
+    resources :addresses, only: [:index]
+  end
+
+  namespace :admin do
+    root 'sessions#new'
+    resources :sessions
+    resources :categories
+    resources :products do
+      resources :product_images, only: [:index, :create, :destroy, :update]
+    end
+  end
+end
