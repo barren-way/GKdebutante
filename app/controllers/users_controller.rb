4673011b62942085b06a24d4553a3c27cf569ab1
EddieAndHisCpp <72387839+EddieAndHisCpp@users.noreply.github.com>
2021-01-04 21:04:13 +0800

    手机验证码注册登录功能开发

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @is_using_email = true
+    @user = User.new
+  end
+
+  def create
+    @is_using_email = (params[:user] and !params[:user][:email].nil?)
+
+    @user = User.new(params.require(:user)
+      .permit(:email, :password, :password_confirmation, :cellphone, :token))
+    @user.uuid = session[:user_uuid]
+
+    if @user.save
+      flash[:notice] = "注册成功，请登录"
+      redirect_to new_session_path
+    else
+      render action: :new
+    end
+  end
+
+end
